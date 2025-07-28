4b3993be51a6fa1ed8a1eaee4cfe3c7b7dca046b
badalsah <badalsah8541@gmail.com>
2025-07-28 07:59:51 +0530

    added frontend live url

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://ai-image-search-engine.vercel.app",
+  ],
 };
 
 app.use(cors(corsOptions));
